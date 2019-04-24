6a188859572e2a4f33400785e424f7ed387e8a19
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-24 16:47:45 +0300

    Sending to the icon.js array of string that contains urls of images srcs Relates #10

--- src/components/Icons/Icons.js
+++ src/components/Icons/Icons.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import Icon from '../Icon/Icon'
+import React from "react";
+import Icon from "../Icon/Icon";
 
 export default class Icons extends React.Component {
   render() {
-    const { display } = this.props;
-    return (
-      <div className="icons--container" style={{display:display}}>
-        <Icon />
-        <Icon />
-        <Icon />
-        <Icon />
-      </div>
+    const { display, categoryData, chosenCat } = this.props;
 
-    )
+    let currentCategory = "";
+    if (chosenCat == "Social Medias") currentCategory = "SocialMedia";
+    else if (chosenCat == "News") currentCategory = "News";
+    else if (chosenCat == "Search Engines") currentCategory = "SearchEngines";
+    else currentCategory = "Static";
+
+    let iconsArray = [];
+
+    if (categoryData[currentCategory])
+      iconsArray = categoryData[currentCategory].icons;
+    
+
+    return (
+      <div className="icons--container" style={{ display: display }}>
+        {
+          iconsArray != undefined && (
+          <>
+            <Icon imgSrc={iconsArray[0]} />
+            <Icon imgSrc={iconsArray[1]} />
+            <Icon imgSrc={iconsArray[2]} />
+            <Icon imgSrc={iconsArray[3]} />
+          </>
+        )}
+      </div>
+    );
   }
 }
