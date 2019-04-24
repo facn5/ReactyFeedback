d616d7f6e1fc9ae4718d3e9af0c9c0b4c0d8ecf3
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-24 15:40:52 +0300

    prettier

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Header.css';
 
 export default class Header extends React.Component {
 
   render() {
     return (
+      <div className="site--header">
       <h1>My sexy header</h1>
+      </div>
     );
   }
 }
