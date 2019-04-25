8e88511dcd5e18b0fd94b665ed9797e106792379
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-25 11:01:04 +0300

    Added submitBtn component that submits the user input of textarea and rating value to localStorage Relates #7, #14

---
+++ src/components/SubmitBtn/SubmitBtn.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default class SubmitBtn extends React.Component {
+  handleOnClick = () => {
+    if (this.props.InputFeedVal && this.props.RatingVal) {
+      localStorage.setItem(
+        this.props.chosenPlatform,
+        `Msg:${this.props.InputFeedVal}&Rate:${this.props.RatingVal}`
+      );
+
+      setTimeout(() => {
+        this.props.exitForm('none');
+      }, 1000);
+
+    } else alert("Please finish your feedback!");
+  };
+  render() {
+    return <button onClick={this.handleOnClick}>Submit your feedback</button>;
+  }
+}
