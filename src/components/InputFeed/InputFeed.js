870c4373e73fdbaace4e3083d48db177c132777b
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-25 11:04:40 +0300

    Added function that sends the user input value of textarea to feedbackform Relates #14

--- src/components/InputFeed/InputFeed.js
+++ src/components/InputFeed/InputFeed.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-export default class InputFeed extends React.Component{
-  render(){
-    return (<input placeholder="Add feedback"/>) 
+export default class InputFeed extends React.Component {
+
+userInput = (event) => {
+  this.props.onChange(event.target.value);
+
+}
+
+  render() {
+    return <textarea onChange={this.userInput} placeholder="Add feedback" />;
   }
 }
