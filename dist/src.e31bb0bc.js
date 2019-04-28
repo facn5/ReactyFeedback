3710329b066f78bbea4c1bdcc35929eb93df2e3f
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-28 11:12:56 +0300

    Dist updated after changing files

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -25834,98 +25834,64 @@ require("./Header.css");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+var Header = function Header() {
+  return _react.default.createElement("div", {
+    className: "site--header"
+  }, _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("span", {
+    className: "tr"
+  }), _react.default.createElement("h1", null, "Reacty Feedback"));
+};
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-var Header =
-/*#__PURE__*/
-function (_React$Component) {
-  _inherits(Header, _React$Component);
-
-  function Header() {
-    _classCallCheck(this, Header);
-
-    return _possibleConstructorReturn(this, _getPrototypeOf(Header).apply(this, arguments));
-  }
-
-  _createClass(Header, [{
-    key: "render",
-    value: function render() {
-      return _react.default.createElement("div", {
-        className: "site--header"
-      }, _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("span", {
-        className: "tr"
-      }), _react.default.createElement("h1", null, "Reacty Feedback"));
-    }
-  }]);
-
-  return Header;
-}(_react.default.Component);
-
-exports.default = Header;
+var _default = Header;
+exports.default = _default;
 },{"react":"../node_modules/react/index.js","./Header.css":"components/Header/Header.css"}],"components/CateButton/CateButton.js":[function(require,module,exports) {
 "use strict";
 
@@ -26845,52 +26811,14 @@ require("./Review.css");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+var Review = function Review(props) {
+  return _react.default.createElement("div", {
+    className: "Reviewd-item--container"
+  }, _react.default.createElement("h3", null, "Platform: ", props.Platform), _react.default.createElement("p", null, "Your feedback: ", props.Msg), _react.default.createElement("hr", null), _react.default.createElement("p", null, "Stars: ", rops.Stars));
+};
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-var Review =
-/*#__PURE__*/
-function (_React$Component) {
-  _inherits(Review, _React$Component);
-
-  function Review() {
-    _classCallCheck(this, Review);
-
-    return _possibleConstructorReturn(this, _getPrototypeOf(Review).apply(this, arguments));
-  }
-
-  _createClass(Review, [{
-    key: "render",
-    value: function render() {
-      var _this$props = this.props,
-          Platform = _this$props.Platform,
-          Msg = _this$props.Msg,
-          Stars = _this$props.Stars;
-      return _react.default.createElement("div", {
-        className: "Reviewd-item--container"
-      }, _react.default.createElement("h3", null, "Platform: ", Platform), _react.default.createElement("p", null, "Your feedback: ", Msg), _react.default.createElement("hr", null), _react.default.createElement("p", null, "Stars: ", Stars));
-    }
-  }]);
-
-  return Review;
-}(_react.default.Component);
-
-exports.default = Review;
+var _default = Review;
+exports.default = _default;
 },{"react":"../node_modules/react/index.js","./Review.css":"components/Review/Review.css"}],"components/Reviews/Reviews.js":[function(require,module,exports) {
 "use strict";
 
